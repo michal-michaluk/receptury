d8654401b632f3a9a23155655b7a8e9a82e7cc63
Michal MIchaluk <michal.michaluk@bottega.com.pl>
2022-11-15 12:55:04 +0100

    Zadanie 4.

---
+++ src/main/java/devices/configuration/device/Device.java
@@ -0,0 +1,21 @@
+package devices.configuration.device;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public
+class Device {
+    final String deviceId;
+
+    private OpeningHours openingHours;
+    private Settings settings;
+
+    void updateOpeningHours(OpeningHours openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    void updateSettings(Settings settings) {
+        this.settings = this.settings.merge(settings);
+    }
+
+}
