2bff1100cdbdc61f4447abddcc665309713b8b2f
Michal MIchaluk <michal.michaluk@bottega.com.pl>
2024-12-04 12:57:35 +0100

    cleanup

--- src/main/java/devices/configuration/search/DeviceReadsController.java
+++ src/main/java/devices/configuration/search/DeviceReadsController.java
@@ -1,6 +1,5 @@
 package devices.configuration.search;
 
-import devices.configuration.auth.Identity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,28 +21,20 @@ class DeviceReadsController {
 
     @GetMapping(path = "/devices", params = {"page", "size"},
             produces = "application/vnd.device.summary+json")
-    Page<DeviceSummary> getSummary(String operator, Pageable pageable, Identity identity) {
-        if (!identity.operators().contains(operator)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
+    Page<DeviceSummary> getSummary(String operator, Pageable pageable) {
         return reads.querySummary(operator, pageable);
     }
 
     @GetMapping(path = "/devices",
             produces = "application/vnd.device.pin+json")
-    List<DevicePin> getPins(String operator, Identity identity) {
-        if (!identity.operators().contains(operator)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
+    List<DevicePin> getPins(String operator) {
         return reads.queryPins(operator);
     }
 
     @GetMapping(path = "/devices/{deviceId}",
             produces = APPLICATION_JSON_VALUE)
-    DeviceDetails getDetails(@PathVariable String deviceId, Identity identity) {
+    DeviceDetails getDetails(@PathVariable String deviceId) {
         return reads.queryDetails(deviceId)
-                .filter(device -> device.configuration() != null)
-                .filter(device -> identity.matches(device.configuration().ownership()))
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.FORBIDDEN));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
